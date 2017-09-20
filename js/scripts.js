b62fb7ab480f598d7a658f219d5793e1626d5874
Gyles Batara and Hansen Hansen <gyles@>
2017-09-20 11:50:51 -0700

    add spec functionality for qu

--- js/scripts.js
+++ js/scripts.js
@@ -4,45 +4,31 @@ $(document).ready(function() {
     var wordsInput = $("#input").val();
     var vowels = ["a", "A", "e", "E", "i", "I", "u", "U", "o", "O"];
     var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
     value = true
 
-    // numbers.forEach(function(number) {
-    //     debugger;
-    //     if (wordsInput.indexOf(number) > 0){
-    //       vowels.forEach(function(vowel) {
-    //         if (wordsInput.indexOf(vowel) = 0) {
-    //         alert(wordsInput+"way")
-    //       };
-    //       });
-    //
-    //     }
-    // });
-    //
-  //   numbers.forEach(function(number) {
-  //     // debugger;
-  //     if (wordsInput.charAt(0) === number && value === true) {
-  //       alert("Please enter a valid word.");
-  //       value = false;
-  //     } else {
-  //     };
-  //   });
-  // });
+    // debugger;
+
   numbers.forEach(function(number) {
     if (wordsInput.charAt(0) == number && value === true) {
-      alert('Please enter alphabetical words only.')
+      alert('Please enter valid characters.')
       value = false;
     }
   });
 
+  if(wordsInput.charAt(0) === "q" && wordsInput.charAt(1) ==="u" && value === true){
+    wordsInput = wordsInput + wordsInput.charAt(0) + wordsInput.charAt(1);
+    wordsInput += "ay";
+    wordsInput = wordsInput.substring(2);
+    alert(wordsInput);
+    value = false;
+
+}
   vowels.forEach(function(vowel) {
-    if (wordsInput.charAt(0) === vowel) {
+    if (wordsInput.charAt(0) === vowel && value === true) {
       wordsInput += "way";
       alert(wordsInput)
-    } else if (wordsInput.charAt(0) !== vowel && wordsInput.charAt(1) === vowel) {
-      wordsInput += wordsInput.charAt(0);
-      wordsInput += "ay";
-      wordsInput = wordsInput.substr(1);
-      alert(wordsInput);
+      value = false;
     }
   });
 
@@ -52,6 +38,17 @@ $(document).ready(function() {
       wordsInput += "ay";
       wordsInput = wordsInput.substr(1);
       alert(wordsInput);
+      value = false;
+    }
+  });
+
+  vowels.forEach(function(vowel) {
+    if (wordsInput.charAt(0) !== vowel && wordsInput.charAt(1) === vowel && value === true) {
+      wordsInput += wordsInput.charAt(0);
+      wordsInput += "ay";
+      wordsInput = wordsInput.substr(1);
+      alert(wordsInput);
+      value = false;
     }
   });
 
@@ -67,25 +64,3 @@ $(document).ready(function() {
 
 });
 });
-
-  // vowels.forEach(function(vowel) {
-  //
-  //   if (wordsInput === vowel && value === true) {
-  //     // && wordsInput.charAt(1) === vowel) {
-  //     wordsInput += "way";
-  //     alert(wordsInput);
-  //     value = false
-  //   } else if (wordsInput.charAt(0) !== vowel && wordsInput.charAt(1) === vowel && value === true) {
-  //     wordsInput += wordsInput.charAt(0);
-  //     wordsInput += "ay";
-  //     wordsInput = wordsInput.substr(1);
-  //     alert(wordsInput);
-  //     value = false;
-  //   } else if (wordsInput.charAt(0) !== vowel && wordsInput.charAt(1) !== vowel && value === true) {
-  //     wordsInput = wordsInput + wordsInput.charAt(0) + wordsInput.charAt(1);
-  //     wordsInput += "ay";
-  //     wordsInput = wordsInput.substr(2);
-  //     alert(wordsInput);
-  //     value = false;
-  //   }
-  // });
